a1a9c966785a2f85aec5d45d2239a154726c084a
bachir112 <bachir.nasser1@gmail.com>
2021-05-03 14:10:32 +0200

    units and statuses fixes

---
+++ Inventory.WebApplication/Controllers/UnitsController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Inventory.DataObjects.EDM;
+using Microsoft.AspNet.Identity;
+
+namespace Inventory.WebApplication.Controllers
+{
+    public class UnitsController : Controller
+    {
+        private InventoryEntities db = new InventoryEntities();
+
+        // GET: Units
+        public ActionResult Index()
+        {
+            ViewBag.PageManagement = Global.Global.AllowedPages(User.Identity.GetUserId());
+            if (Global.Global.isAllowed(User.Identity.GetUserId(), "Units"))
+            {
+                return View(db.Units.ToList());
+            }
+            else
+            {
+                return RedirectToAction("NotAuthorized", "Home");
+            }
+        }
+
+        // GET: Units/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Unit unit = db.Units.Find(id);
+            if (unit == null)
+            {
+                return HttpNotFound();
+            }
+            return View(unit);
+        }
+
+        // GET: Units/Create
+        public ActionResult Create()
+        {
+            ViewBag.PageManagement = Global.Global.AllowedPages(User.Identity.GetUserId());
+            if (Global.Global.isAllowed(User.Identity.GetUserId(), "Units"))
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("NotAuthorized", "Home");
+            }
+        }
+
+        // POST: Units/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        public ActionResult Create([Bind(Include = "Id,Name,Description")] Unit unit)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Units.Add(unit);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(unit);
+        }
+
+        // GET: Units/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            ViewBag.PageManagement = Global.Global.AllowedPages(User.Identity.GetUserId());
+            if (Global.Global.isAllowed(User.Identity.GetUserId(), "Units"))
+            {
+                if (id == null)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+                Unit unit = db.Units.Find(id);
+                if (unit == null)
+                {
+                    return HttpNotFound();
+                }
+                return View(unit);
+            }
+            else
+            {
+                return RedirectToAction("NotAuthorized", "Home");
+            }
+        }
+
+        // POST: Units/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        public ActionResult Edit([Bind(Include = "Id,Name,Description")] Unit unit)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(unit).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(unit);
+        }
+
+        // GET: Units/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            ViewBag.PageManagement = Global.Global.AllowedPages(User.Identity.GetUserId());
+            if (Global.Global.isAllowed(User.Identity.GetUserId(), "Units"))
+            {
+                if (id == null)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+                Unit unit = db.Units.Find(id);
+                if (unit == null)
+                {
+                    return HttpNotFound();
+                }
+                return View(unit);
+            }
+            else
+            {
+                return RedirectToAction("NotAuthorized", "Home");
+            }
+        }
+
+        // POST: Units/Delete/5
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Unit unit = db.Units.Find(id);
+            db.Units.Remove(unit);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
